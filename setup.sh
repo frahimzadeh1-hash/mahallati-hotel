e8943b5c3500078c2a18696937c3991dbfa86b52
frahimzadeh1-hash <f.rahimzadeh1@gmail.com>
2026-06-27 14:39:33 +0330

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-pip install --upgrade pip
-pip install --no-cache-dir numpy==1.23.5
-pip install --no-cache-dir pandas==1.5.3
-pip install --no-cache-dir -r requirements.txt
+pip install --upgrade pip setuptools wheel
+pip install --no-cache-dir --only-binary :all: -r requirements.txt
